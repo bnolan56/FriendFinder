df192ab96ad779c9a0d31f4aadfb0a3c1a9e1795
Brad Nolan <bnolan56@gmail.com>
2018-08-20 15:40:51 -0600

    more commits

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,168 @@
 // Exports for data arrays
 // ===========================================
 
-exports.owFriends = [
-  {
-  "name": "",
-  "photoUrl": "",
-  "values": ""
-  }
-]
+// Data structure that will store the friends data
+let heroesArray = [
+	{
+		name: 'Tracer',
+		photoUrl: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		values: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Hanzo',
+		photoUrl: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
+		values: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Genji',
+		photoUrl: 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
+		values: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Zenyatta',
+		photoUrl: 'https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg',
+		values: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Reinhardt',
+		photoUrl: 'http://4.bp.blogspot.com/-HTvSYzA-pO4/UgQb4Zh_u0I/AAAAAAAAEuI/XwhtogT_1tA/s1600/3+cute2.jpg',
+		values: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Widowmaker',
+		photoUrl: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+		values: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Mercy',
+		photoUrl: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		values: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Reaper',
+		photoUrl: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+		values: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Junkrat',
+		photoUrl: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
+		values: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Mei',
+		photoUrl: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		values: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = heroesArray;
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,11 +1,61 @@
-const friendData = require('../data/friends');
+const heroData = require('../data/friends.js');
+const path = require('path');
 
-module.exports = function (app) {
-  app.get('/api/friends', function (req, res, next) {
-    res.json(friendData)
-  })
-  app.post('/api/friends', function (req, res, next) {
-    console.log(req.body)
-    res.json(friendData)
-  })  
+// module.exports = function (app) {
+//   app.get('/api/friends', function (req, res, next) {
+//     res.json(heroData)
+//   })
+//   app.post('/api/friends', function (req, res, next) {
+//     console.log(req.body)
+//     res.json(heroData)
+//   })
+// };
+
+module.exports = function(app) {
+	// console.log('___ENTER apiRoutes.js___');
+
+	// Total list of friend entries
+	app.get('/api/friends', function(req, res) {
+		res.json(heroData);
+	});
+
+	// Add new friend entry
+	app.post('/api/friends', function(req, res) {
+		// Capture the user input object
+		let userInput = req.body;
+		// console.log('userInput = ' + JSON.stringify(userInput));
+
+		let userResponses = userInput.values;
+		// console.log('userResponses = ' + userResponses);
+
+		// Compute best friend match
+		let matchName = '';
+		let matchImage = '';
+		let totalDifference = 10000; // Make the initial value big for comparison
+
+		// Examine all existing friends in the list
+		for (let i = 0; i < heroData.length; i++) {
+			// console.log('friend = ' + JSON.stringify(friends[i]));
+
+			// Compute differences for each question
+			let diff = 0;
+			for (let j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(heroData[i].values[j] - userResponses[j]);
+			}
+			// console.log('diff = ' + diff);
+
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
+				totalDifference = diff;
+				matchName = heroData[i].name;
+				matchImage = heroData[i].photoUrl;
+			}
+		}
+
+		// Add new user
+		heroData.push(userInput);
+
+		// Send appropriate response
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	});
 };
